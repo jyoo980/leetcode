b97f9641ceb19a399755efb027876c779c4c3ae0
James Yoo <jyoo980@gmail.com>
2018-01-06 20:52:19 -0800

    Added reversal function

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,2 +1,29 @@
 public class ReverseString {
+
+    /**
+     * Write a function that takes a string as input and returns the string reversed.
+     * If the input is null, return null.
+     *
+     * Time: O(n)
+     * Space: O(n)
+     *
+     */
+    public String reverse(String str) {
+        if (str.length() <= 1) {
+            return str;
+        } else {
+            char[] arr = str.toCharArray();
+
+            int begin = 0;
+            int end = arr.length - 1;
+            while (end > begin) {
+                char tmp = arr[begin];
+                arr[begin] = arr[end];
+                arr[end] = tmp;
+                end--;
+                begin++;
+            }
+            return new String(arr);
+        }
+    }
 }
