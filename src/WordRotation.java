4a19a6089d8d6e2697a7c7976a2e2e624f403eda
James Yoo <jyoo980@gmail.com>
2018-01-07 17:10:07 -0800

    Added WordRotation.java

--- src/WordRotation.java
+++ src/WordRotation.java
@@ -1,2 +1,23 @@
 public class WordRotation {
+
+    /**
+     * Given two strings s and t, determine if t is a "rotation" of s.
+     *
+     * For example, s = "waterbottle", t = "lewaterbott", return true
+     *
+     */
+    public static boolean validRotation(String s, String t) {
+        if  (s.length() != t.length()) {
+            return false;
+        } else {
+            StringBuilder tBuilder = new StringBuilder(t);
+            tBuilder.append(tBuilder);
+
+            String tt = tBuilder.toString();
+
+            return tt.contains(s);
+        }
+    }
+
+
 }
