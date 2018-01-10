e6502f845a36a0961d65022ee94ae46a07e1b5ec
James Yoo <jyoo980@gmail.com>
2018-01-09 22:58:38 -0800

    Finished testing StringSegements.java

--- src/StringSegments.java
+++ src/StringSegments.java
@@ -1,2 +1,29 @@
 public class StringSegments {
+
+    /**
+     * Count the number of segments in a string, where a segment is defined to be a contiguous
+     * sequence of non-space characters.
+     *
+     * Input: "Hello, my name is John"
+     * Output: 5
+     *
+     */
+    public static int stringSegments(String s) {
+        char[] strArray = s.toCharArray();
+        int segCounter = 0;
+
+        for (char c : strArray) {
+            if (c == ' ') {
+                segCounter++;
+            }
+        }
+
+        return segCounter + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringSegments("Hello"));
+        System.out.println(stringSegments("Hello, my name is John"));
+    }
+
 }
