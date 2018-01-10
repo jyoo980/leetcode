47158b510d1496a00fbe5cf7dda2c3ca9e7c445c
James Yoo <jyoo980@gmail.com>
2018-01-09 22:38:49 -0800

    Finished KthLargestElement.java

--- src/KthLargestElement.java
+++ src/KthLargestElement.java
@@ -1,3 +1,23 @@
+import java.util.Arrays;
+
 public class KthLargestElement {
-    
+
+    /**
+     * Find the kth largest element in an unsorted array. Note that it is the kth largest
+     * element in the sorted order, not the kth distinct element.
+     * For example,
+     * Given [3,2,1,5,6,4] and k = 2, return 5.
+     */
+    public static int kthLargest(int[] array, int k) {
+
+        Arrays.sort(array);
+        int index = array.length - k;
+        return (index < 0) ? -1 : array[index];
+    }
+
+//    public static void main(String[] args) {
+//        int[] test = {3, 2, 1, 5, 6, 4};
+//        System.out.println(kthLargest(test, 2));
+//    }
+
 }
