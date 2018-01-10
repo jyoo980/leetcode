8621be72cd76110b31c269f8275ddb90eb20eadb
James Yoo <jyoo980@gmail.com>
2018-01-09 22:32:07 -0800

    Added KthLargestElement.java

--- src/UniqueChar.java
+++ src/UniqueChar.java
@@ -1,2 +1,33 @@
-public class UniqueChar {
-}
+//public class UniqueChar {
+//
+//    /**
+//     * Given a string, find the first non-repeating character in it and return it's index.
+//     * If it doesn't exist, return -1.
+//     *
+//     * s = "leetcode"
+//     * return 0.
+//     *
+//     * s = "loveleetcode",
+//     * return 2.
+//     *
+//     */
+//    public static int uniqueCharIndex(String s) {
+//        int unique = 0;
+//        char[] charArray = s.toCharArray();
+//        char uniqueSoFar = charArray[0];
+//
+//        for (int i = 0; i < charArray.length - 1; i++) {
+//            if (charArray[i] != uniqueSoFar) {
+//
+//            }
+//        }
+//
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(uniqueCharIndex("leetcode"));
+//        System.out.println(uniqueCharIndex("loveleetcode"));
+//    }
+//}
