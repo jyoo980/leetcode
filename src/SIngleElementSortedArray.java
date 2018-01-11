55546f1c0a9e4f85d3f14a4a0b7dc6b3569fd537
James Yoo <jyoo980@gmail.com>
2018-01-10 21:08:04 -0800

    Finished SingleElementSortedArray.java

--- src/SIngleElementSortedArray.java
+++ src/SIngleElementSortedArray.java
@@ -1,2 +1,30 @@
 public class SIngleElementSortedArray {
+
+    /**
+     * Given a sorted array consisting of only integers where every element appears twice except for one element which
+     * appears once. Find this single element that appears only once.
+     *
+     * Input: [1,1,2,3,3,4,4,8,8]
+     * Output: 2
+     *
+     */
+    public static int singleElementSortedArray(int[] array) {
+
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[i + 1] && array[i + 1] != array[i + 2]) {
+                return array[i + 1];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] test = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+        int[] test2 = {1, 1, 2, 3, 3};
+        int[] test3 = {};
+        System.out.println(singleElementSortedArray(test));
+        System.out.println(singleElementSortedArray(test2));
+        System.out.println(singleElementSortedArray(test3));
+
+    }
 }
