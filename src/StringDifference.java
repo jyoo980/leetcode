b5ae8c2c68637ab87f3c1c08fe65e68f6c9856c1
James Yoo <jyoo980@gmail.com>
2018-01-12 00:02:49 -0800

    Finished testing StringDifference.java

--- src/StringDifference.java
+++ src/StringDifference.java
@@ -13,23 +13,18 @@ public class StringDifference {
      *
      */
     public static String getAddedLetter(String s, String t) {
-        if (s.length() <= 1 || t.length() <= 1) {
-            return "";
-        } else {
-
-            for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) != t.charAt(i)) {
                     return Character.toString(t.charAt(i));
                 }
             }
 
             return Character.toString(t.charAt(s.length()));
-        }
     }
 
-    public static void main(String[] args) {
-        System.out.println(getAddedLetter("abcd", "abcde"));
-    }
-
+//    public static void main(String[] args) {
+//        System.out.println(getAddedLetter("s", "at"));
+//        System.out.println(getAddedLetter("abcd", "abcde"));
+//    }
 
 }
