43753b05f1a58aebe767c751b5208193bcd5ea56
James Yoo <jyoo980@gmail.com>
2018-01-11 23:05:58 -0800

    Switched to FindArrayDuplicates.java

---
+++ src/FindArrayDuplicates.java
@@ -0,0 +1,15 @@
+public class FindArrayDuplicates {
+
+    /**
+     * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice
+     * and others appear once. Find all the elements that appear twice in this array.
+     *
+     * Input: [4,3,2,7,8,2,3,1]
+     * Output: [2,3]
+     *
+     */
+    public static int[] getDuplicateNums(int[] array) {
+        return null;
+    }
+
+}
