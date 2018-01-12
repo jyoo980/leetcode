2ff75b7cb428f2e3bcb242a29923ead8dba6d80a
James Yoo <jyoo980@gmail.com>
2018-01-11 23:00:10 -0800

    Added MissingNumArray.java

--- src/SIngleElementSortedArray.java
+++ src/SIngleElementSortedArray.java
@@ -18,13 +18,16 @@ public class SIngleElementSortedArray {
         return -1;
     }
 
-    public static void main(String[] args) {
-        int[] test = {1, 1, 2, 3, 3, 4, 4, 8, 8};
-        int[] test2 = {1, 1, 2, 3, 3};
-        int[] test3 = {};
-        System.out.println(singleElementSortedArray(test));
-        System.out.println(singleElementSortedArray(test2));
-        System.out.println(singleElementSortedArray(test3));
+//    public static void main(String[] args) {
+//        int[] test = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+//        int[] test2 = {1, 1, 2, 3, 3};
+//        int[] test3 = {};
+//        int[] test4 = {3, 3, 7, 7, 10, 11, 11};
+//        System.out.println(singleElementSortedArray(test));
+//        System.out.println(singleElementSortedArray(test2));
+//        System.out.println(singleElementSortedArray(test3));
+//        System.out.println(singleElementSortedArray(test4));
+//
+//    }
 
-    }
 }
