48c7c0b05d92ad31a53b70a7fa3d36cc140d573f
James Yoo <jyoo980@gmail.com>
2018-01-20 15:00:08 -0800

    Added testcases for MaxThreeProduct.java

--- src/MaxThreeProduct.java
+++ src/MaxThreeProduct.java
@@ -1,2 +1,21 @@
 public class MaxThreeProduct {
+
+    /**
+     * Given an integer array, find three numbers whose product is maximum and output the maximum product.
+     *
+     * E.g. Input: [1,2,3]   -> Output: 6
+     *      Input: [1,2,3,4] -> Output: 24
+     *
+     */
+    public static int maxThreeProduct(int[] nums) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = {1,2,3};
+        int[] test2 = {1,2,3,4};
+        System.out.println(maxThreeProduct(test1));
+        System.out.println(maxThreeProduct(test2));
+    }
+
 }
