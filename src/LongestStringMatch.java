000208ca96ac601961378fd1801be2c7d1150b5b
James Yoo <jyoo980@gmail.com>
2018-01-20 15:32:12 -0800

    Added LongestStringMatch.java

---
+++ src/LongestStringMatch.java
@@ -0,0 +1,19 @@
+public class LongestStringMatch {
+
+    /**
+     * Given two strings s and t, return the length of the string sequence they have in
+     * common starting from the beginning.
+     *
+     * E.g. "abcdef", "abcdefgh" -> 6
+     *      "a", "ab"            -> 1
+     *
+     */
+    public static int longestCommonLen(String s, String t) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
