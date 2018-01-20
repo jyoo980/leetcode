dd1de71a908a4e3d6e0ed218eb8b6e62f5d680fc
James Yoo <jyoo980@gmail.com>
2018-01-19 22:49:37 -0800

    Added method stubs

--- src/TwiceOthers.java
+++ src/TwiceOthers.java
@@ -1,2 +1,23 @@
 public class TwiceOthers {
+
+    /**
+     * In a given integer array nums, there is always exactly one largest element.
+     * Find whether the largest element in the array is at least twice as much as every other number in the array.
+     * If it is, return the index of the largest element, otherwise return -1.
+     *
+     * Input: nums = [3, 6, 1, 0]
+     * Output: 1
+     *
+     */
+    public static int twiceOthers(int[] nums) {
+        return -1;
+    }
+
+    private static int getMax(int[] nums) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
