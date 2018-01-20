c9f91c0cd93bfdff9773b78d628716dc05ac6ef1
James Yoo <jyoo980@gmail.com>
2018-01-19 22:37:17 -0800

    Added test cases

--- src/ValidParens.java
+++ src/ValidParens.java
@@ -1,2 +1,19 @@
 public class ValidParens {
+
+    /**
+     * Given an input string str, return true if str is a valid string of parenthesis
+     * E.g. "{}()[]" -> return true
+     *      "{({})"  -> return true
+     *      "(]()"   -> return false
+     */
+    public static boolean validParens(String str) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validParens("{}()[]"));
+        System.out.println(validParens("{({})"));
+        System.out.println(validParens("(]()"));
+    }
+    
 }
