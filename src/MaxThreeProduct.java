4ad0b8c767d8c271e0a3e6596263149201f11b3a
James Yoo <jyoo980@gmail.com>
2018-01-20 15:12:00 -0800

    Completed MaxThreeProduct.java

--- src/MaxThreeProduct.java
+++ src/MaxThreeProduct.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MaxThreeProduct {
 
     /**
@@ -8,14 +10,29 @@ public class MaxThreeProduct {
      *
      */
     public static int maxThreeProduct(int[] nums) {
-        return -1;
+        int maxProduct = 1;
+        if (nums.length == 0) {
+            return -1;
+        } else if (nums.length == 3) {
+            for (int num : nums) {
+                maxProduct *= num;
+            }
+            return maxProduct;
+        }
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i > nums.length - 4; i--) {
+            maxProduct *= nums[i];
+        }
+        return maxProduct;
     }
 
     public static void main(String[] args) {
         int[] test1 = {1,2,3};
         int[] test2 = {1,2,3,4};
+        int[] test3 = {3, 2, 6, 0, 1, 3};
         System.out.println(maxThreeProduct(test1));
         System.out.println(maxThreeProduct(test2));
+        System.out.println(maxThreeProduct(test3));
     }
 
 }
