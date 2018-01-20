4e7693bc85173ab50f88247844bd9203560310d5
James Yoo <jyoo980@gmail.com>
2018-01-20 15:39:51 -0800

    Created method stubs

--- src/MergeSortedLists.java
+++ src/MergeSortedLists.java
@@ -1,2 +1,17 @@
+import java.util.List;
+
 public class MergeSortedLists {
+
+    /**
+     * Merge two sorted linked lists and return it as a new list. The new list should be made by
+     * splicing together the nodes of the first two lists.
+     *
+     */
+    public static List<Integer> mergeLists(List<Integer> list1, List<Integer> list2) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
