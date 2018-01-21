4d4556ef07899fda97249b0ce3212028bbeb107d
James Yoo <jyoo980@gmail.com>
2018-01-21 14:37:44 -0800

    Added method stubs

--- src/ArrayProduct.java
+++ src/ArrayProduct.java
@@ -1,2 +1,21 @@
+import java.util.List;
+
 public class ArrayProduct {
+
+    /**
+     *
+     * Given an array of n integers where n > 1, nums, return an array output such that output[i]
+     * is equal to the product of all the elements of nums except nums[i].
+     *
+     * E.g. [1,2,3,4], return [24,12,8,6]
+     *
+     */
+    public static List<Integer> arrayProduct(int[] nums) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = {1,2,3,4};
+        System.out.println(arrayProduct(test1));
+    }
 }
