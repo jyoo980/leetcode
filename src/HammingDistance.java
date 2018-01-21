9897b707431a4972c39b4ead5a234e76ae594192
James Yoo <jyoo980@gmail.com>
2018-01-20 19:14:22 -0800

    Added method stubs

--- src/HammingDistance.java
+++ src/HammingDistance.java
@@ -1,2 +1,19 @@
 public class HammingDistance {
+
+    /**
+     * The Hamming distance between two integers is the number of positions at which the
+     * corresponding bits are different. Given two integers x and y, calculate the Hamming distance.
+     *
+     * E.g. x = 1, y = 4 -> 2
+     *      x = 1, y = 2 -> 2
+     *
+     */
+    public static int hammingDistance(int x, int y) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
