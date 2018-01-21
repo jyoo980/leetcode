207b004252fd941b898822e53f0b43d24ef67890
James Yoo <jyoo980@gmail.com>
2018-01-21 14:50:59 -0800

    Created method stubs

--- src/PowerN.java
+++ src/PowerN.java
@@ -1,2 +1,17 @@
 public class PowerN {
+
+    /**
+     * Implement pow(x, n).
+     *
+     * E.g. Input: 2.00000, 10 -> 1024.00000
+     *             2.10000, 3  -> 9.26100
+     *
+     */
+    public static double pow(int x, int n) {
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
