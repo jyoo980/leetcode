af3049e7dd36bdd83da8ae34fe115e31c57f0d1c
James Yoo <jyoo980@gmail.com>
2018-01-21 20:42:46 -0800

    Added method stubs

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -1,2 +1,19 @@
+import java.util.List;
+
 public class RemoveDuplicates {
+
+    /**
+     * Given a sorted array, remove the duplicates in-place such that each
+     * element appear only once and return the new length.
+     *
+     * E.g. [1, 1, 3, 4] -> return 2
+     *
+     */
+    public static int removeDuplicates(List<Integer> array) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
