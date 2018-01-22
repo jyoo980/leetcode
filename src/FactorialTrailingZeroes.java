350ac790b14f2819cf1e1b8d05de1c64208ff9f5
James Yoo <jyoo980@gmail.com>
2018-01-21 21:19:23 -0800

    Completed FactorialTrailingZeroes.java

--- src/FactorialTrailingZeroes.java
+++ src/FactorialTrailingZeroes.java
@@ -5,14 +5,32 @@ public class FactorialTrailingZeroes {
      *
      */
     public static int trailingZeroes(int n) {
-        return -1;
+        int nFactorial = factorial(n);
+        int trailingZeros = 0;
+        StringBuilder nFactorialString = new StringBuilder(Integer.toString(nFactorial));
+        nFactorialString.reverse();
+
+        for (int i = 0; i < nFactorialString.length(); i++) {
+            if (nFactorialString.charAt(i) == '0') {
+                trailingZeros++;
+            } else if (nFactorialString.charAt(i) != '0') {
+                return trailingZeros;
+            }
+        }
+
+        return trailingZeros;
     }
 
     private static int factorial(int num) {
-        return -1;
+        int result = 1;
+        for (int i = num; i > 0; i--) {
+            result *= i;
+        }
+        return result;
     }
 
-    public static void main(String[] args) {
-
-    }
+//    public static void main(String[] args) {
+//        System.out.println(trailingZeroes(5));
+//    }
+    
 }
