b5dd82122d04f81525aba11c49c086b3ceba7ada
James Yoo <jyoo980@gmail.com>
2018-01-21 21:04:42 -0800

    Added factorial helper function

--- src/FactorialTrailingZeroes.java
+++ src/FactorialTrailingZeroes.java
@@ -1,2 +1,18 @@
 public class FactorialTrailingZeroes {
+
+    /**
+     * Given an integer n, return the number of trailing zeroes in n!.
+     *
+     */
+    public static int trailingZeroes(int n) {
+        return -1;
+    }
+
+    private static int factorial(int num) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
