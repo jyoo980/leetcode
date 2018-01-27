0b49d0d887a9675591a1e2fdba2c26ba2a935ce8
James Yoo <jyoo980@gmail.com>
2018-01-27 15:22:36 -0800

    Completed Function stubs

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -1,2 +1,19 @@
 public class PerfectNumber {
+
+    /**
+     * We define the Perfect Number is a positive integer that is equal to the sum of all its
+     * positive divisors except itself.Now, given an integer n, write a function that returns true
+     * when it is a perfect number and false when it is not.
+     *
+     * E.g. input = 28 -> True, since 28 = 1 + 2 + 4 + 7 + 14
+     *
+     */
+    public static boolean isPerfectNumber(int num) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
 }
