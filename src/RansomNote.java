146f66e909beaf75c0a4c848e3986a26d5fd4c35
James Yoo <jyoo980@gmail.com>
2018-01-27 14:32:20 -0800

    Added test cases/method stubs

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -1,2 +1,21 @@
 public class RansomNote {
+
+    /**
+     * Given an arbitrary ransom note string and another string containing letters from all the magazines,
+     * write a function that will return true if the ransom note can be constructed from the magazines;
+     * otherwise, it will return false.
+     *
+     * E.g. canConstruct("a", "b") -> false
+     *      canConstruct("aa", "ab") -> false
+     *      canConstruct("aa", "aab") -> true
+     */
+    public static boolean canConstruct(String note, String mag) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canConstruct("a", "b"));
+        System.out.println(canConstruct("aa", "ab"));
+        System.out.println(canConstruct("aa", "aab"));
+    }
 }
