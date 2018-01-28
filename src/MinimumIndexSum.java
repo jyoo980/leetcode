ee0163935a9737f0bd7b03cac6e6ffedb25013b1
James Yoo <jyoo980@gmail.com>
2018-01-27 23:03:40 -0800

    Finished MinimumIndexSum.java

--- src/MinimumIndexSum.java
+++ src/MinimumIndexSum.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class MinimumIndexSum {
 
@@ -13,18 +14,37 @@ public class MinimumIndexSum {
      *      return: ["Shogun"]
      */
     public static String[] getLeastIndexSum(String[] array1, String[] array2) {
-        HashMap<String, Integer> commonMap = new HashMap<>();
+        HashMap<Integer, String> commonMap = new HashMap<>();
         for (int i = 0; i < array1.length; i++) {
             int correspondingIndex = getLocation(array1[i], array2);
+            commonMap.put(i + correspondingIndex, array1[i]);
         }
+
+        int min = 1000000;
+        for (Map.Entry<Integer, String> entry : commonMap.entrySet()) {
+            min = (entry.getKey() < min) ? entry.getKey() : min;
+        }
+        String[] results = new String[]{commonMap.get(min)};
+        return results;
     }
 
     private static int getLocation(String s, String[] array2) {
-        return 0;
+        for (int i = 0; i < array2.length; i++) {
+            if (array2[i].equals(s)) {
+                return i;
+            }
+        }
+        return 1000000;     // if not found
     }
 
     public static void main(String[] args) {
-
+        String[] test1 = {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+        String[] test2 = {"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"};
+        String[] result1 = getLeastIndexSum(test1, test2);
+        for (String s : result1) {
+            System.out.println(s);
+        }
     }
 
 }
+
