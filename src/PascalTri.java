ab0baa19894728e4c16446f0d7efd7b68c70f2b2
James Yoo <jyoo980@gmail.com>
2018-01-27 23:15:45 -0800

    Added function stubs for PascalTri.java

--- src/PascalTri.java
+++ src/PascalTri.java
@@ -1,2 +1,17 @@
 public class PascalTri {
+
+    /**
+     * Given numRows, generate the first numRows of Pascal's triangle.
+     *
+     * E.g. numRows =  5
+     *      return = [ [1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1]]
+     */
+    public static int[] pascalTri(int numRows) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
 }
