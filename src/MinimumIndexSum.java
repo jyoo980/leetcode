19a6065adf6d6a8416497239fd95bd5447764bf7
James Yoo <jyoo980@gmail.com>
2018-01-27 22:32:29 -0800

    Test suites finished

--- src/MinimumIndexSum.java
+++ src/MinimumIndexSum.java
@@ -1,2 +1,20 @@
 public class MinimumIndexSum {
+
+    /**
+     * Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a
+     * list of favorite restaurants represented by strings. You need to help them find out
+     * their common interest with the least list index sum.
+     *
+     * E.g. input(s): ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+     *                ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+     *      return: ["Shogun"]
+     */
+    public static String[] getLeastIndexSum(String[] array1, String[] array2) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
 }
