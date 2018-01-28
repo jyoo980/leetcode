9cbc0e38fcff7a9e1cba5c52726f4a1094206f10
James Yoo <jyoo980@gmail.com>
2018-01-27 19:32:49 -0800

    Added method stubs

--- src/LongestUncommonSubs.java
+++ src/LongestUncommonSubs.java
@@ -1,2 +1,18 @@
 public class LongestUncommonSubs {
+
+    /**
+     * Given a group of two strings, you need to find the longest uncommon subsequence of this group of two strings.
+     * The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this
+     * subsequence should not be any subsequence of the other strings.
+     *
+     * E.g. Input: "aba", "cdc" -> 3
+     *             "abcdef", "abcdefg" -> 1
+     *              "gnfguiddfnfd", "gnfgueyzfnfd" -> 3
+     */
+    public static int longestUncommonSub(String s, String t) {
+        int longestUncommonLen = 0;
+        int currentUncommonLen = 0;
+        int len = (s.length() < t.length())? s.length() : t.length();
+        return -1;
+    }
 }
